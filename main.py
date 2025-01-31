f47d3d7b5b70021e42734f7b7d71ed4cee7a83ba
vithiya <i.vithiya@gmail.com>
2025-01-31 16:33:36 +0000

    Added Procfile

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI","sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
